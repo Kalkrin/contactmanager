9de955e130c211a9944f104d7d4607cac22421b8
Kalkrin <blitzkriegx3@gmail.com>
2019-01-25 19:40:52 -0500

    Updates

---
+++ precache-manifest.cea750e958ec0436e056263eee169c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "5382f54f7379e60b1d5f",
+    "url": "/contactmanager/static/js/main.5382f54f.chunk.js"
+  },
+  {
+    "revision": "65392e23339c855b5542",
+    "url": "/contactmanager/static/js/1.65392e23.chunk.js"
+  },
+  {
+    "revision": "5382f54f7379e60b1d5f",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "65392e23339c855b5542",
+    "url": "/contactmanager/static/css/1.a51faf75.chunk.css"
+  },
+  {
+    "revision": "192744321f5b273c9e2fc3830c9aeb08",
+    "url": "/contactmanager/index.html"
+  }
+];
