e794e2fb7ee5179f977aa9eb519819ff5c17d11d
Kalkrin <blitzkriegx3@gmail.com>
2019-01-25 20:08:57 -0500

    Updates

--- precache-manifest.195c425a2fbc6af76a9bdaf898426ce5.js
+++ precache-manifest.195c425a2fbc6af76a9bdaf898426ce5.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
   },
   {
-    "revision": "4fd6372130f4308b68c6",
-    "url": "/contactmanager/static/js/main.4fd63721.chunk.js"
+    "revision": "52992e0a353aa1292281",
+    "url": "/contactmanager/static/js/main.52992e0a.chunk.js"
   },
   {
     "revision": "65392e23339c855b5542",
     "url": "/contactmanager/static/js/1.65392e23.chunk.js"
   },
   {
-    "revision": "4fd6372130f4308b68c6",
+    "revision": "52992e0a353aa1292281",
     "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/css/1.a51faf75.chunk.css"
   },
   {
-    "revision": "7156607406a5c9542dd019afb62d7a57",
+    "revision": "7fb64c4ac3c6dfb7dac808b5389b90cd",
     "url": "/contactmanager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contactmanager/precache-manifest.49c8b4fce31dbcf7574a6d44a478afb8.js"
+  "/contactmanager/precache-manifest.195c425a2fbc6af76a9bdaf898426ce5.js"
 );
 
 workbox.clientsClaim();
